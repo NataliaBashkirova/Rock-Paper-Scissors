3a05b1eb76315f3f37cbbaeb013a55c2f0486eeb
NataliaBashkirova <bashkirova189@gmail.com>
2024-06-26 21:22:00 +0200

    Implement getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+let humanScore = 0,
+    computerScore = 0;
+
+function getComputerChoice()
+{
+    let randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case (0) : 
+        return "Rock";
+        case(1) :
+        return "Paper";
+        case (2) :
+        return "Scissors";
+    }
+    
+}
+
+console.log(getComputerChoice());
+console.log("I'm ready to play Rock-Paper-Scissors.");
+
