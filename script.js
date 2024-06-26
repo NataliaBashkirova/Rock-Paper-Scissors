50ff2355f470797aeb07af9bd23c38c304cf682f
NataliaBashkirova <bashkirova189@gmail.com>
2024-06-26 22:00:18 +0200

    Implement helper function makeStringProper

--- script.js
+++ script.js
@@ -1,39 +1,62 @@
 let humanScore = 0,
-    computerScore = 0;
-let computerChoice,
-    humanChoice;
+  computerScore = 0;
+let computerChoice, 
+  humanChoice;
 
-function getComputerChoice()
-{
-
-    let rocks = 0,
-        papers = 0, 
-        scissors = 0;
-    for (i = 0; i < 100000; i++) {
-        let randomNumber = Math.floor(Math.random() * 3);
-        switch (randomNumber) {
-            case (0) : 
-            rocks++;
-            return "rock";
-            case(1) :
-            papers++;
-            return "paper";
-            case (2) :
-            scissors++;
-            return "scissors";
-            default : 
-            throw new Error("Computer choice out of boundaries.")
-        }    
+function getComputerChoice() {
+  let rocks = 0,
+    papers = 0,
+    scissors = 0;
+  for (i = 0; i < 100000; i++) {
+    let randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+      case 0:
+        rocks++;
+        return "Rock";
+      case 1:
+        papers++;
+        return "Paper";
+      case 2:
+        scissors++;
+        return "Scissors";
+      default:
+        throw new Error("Computer choice out of boundaries.");
     }
+  }
 }
 
 function getHumanChoice() {
+  return makeStringProper(prompt("What will you pick? (rock/paper/scissors)")
+    .trim()
+    .toLowerCase());
+}
 
+function makeStringProper(input) {
+    if (input.length > 0) {
+        return input.slice(0, 1).toUpperCase() + input.slice(1);
+    }
+    else {
+        throw new Error("String is empty or too short.");
+    }
+}
+
+function playRound(computerChoice, humanChoice) {
+    if (computerChoice === humanChoice) {
+        return `It's a draw! Computer chose ${computerChoice} too.`;
+    }
+    else if ( (computerChoice === "rock" && humanChoice === "paper")
+           || (computerChoice === "paper" && humanChoice === "scissors")
+           || (computerChoice === "scissors" && humanChoice === "rock") ) {
+            return `You won! ${humanChoice} beats ${computerChoice}.`;
+        }
+    else {
+        return `You lose! ${computerChoice} beats ${humanChoice}.`
+    }
+    
 }
 
 computerChoice = getComputerChoice();
-humanChoice = prompt("What will you pick? (rock/paper/scissors)").trim().toLowerCase();
+humanChoice = getHumanChoice();
 
-console.log (humanChoice);
+console.log(playRound(computerChoice, humanChoice));
 console.log("I'm ready to play Rock-Paper-Scissors.");
-
