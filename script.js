4c6533e05f10869e8002e06977e35a185b3a79e9
NataliaBashkirova <bashkirova189@gmail.com>
2024-06-26 21:41:53 +0200

    Implement getHumanChoice function

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 let humanScore = 0,
     computerScore = 0;
+let computerChoice,
+    humanChoice;
 
 function getComputerChoice()
 {
-    let randomNumber = Math.floor(Math.random() * 3);
-    switch (randomNumber) {
-        case (0) : 
-        return "Rock";
-        case(1) :
-        return "Paper";
-        case (2) :
-        return "Scissors";
+
+    let rocks = 0,
+        papers = 0, 
+        scissors = 0;
+    for (i = 0; i < 100000; i++) {
+        let randomNumber = Math.floor(Math.random() * 3);
+        switch (randomNumber) {
+            case (0) : 
+            rocks++;
+            return "rock";
+            case(1) :
+            papers++;
+            return "paper";
+            case (2) :
+            scissors++;
+            return "scissors";
+            default : 
+            throw new Error("Computer choice out of boundaries.")
+        }    
     }
-    
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+
+}
+
+computerChoice = getComputerChoice();
+humanChoice = prompt("What will you pick? (rock/paper/scissors)").trim().toLowerCase();
+
+console.log (humanChoice);
 console.log("I'm ready to play Rock-Paper-Scissors.");
 
